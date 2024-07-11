d1e462a5622e97bbc811d982ebe8b4085542f6b2
Ulises Jimenez <ulisesjimenez@google.com>
2024-07-11 15:52:07 -0600

    format readme

--- cloud-functions/beans/main.py
+++ cloud-functions/beans/main.py
@@ -2,6 +2,7 @@ import functions_framework
 from google.cloud import aiplatform
 from google.api_core.exceptions import NotFound
 
+
 @functions_framework.http
 def run_beans_pipeline(request):
     """HTTP Cloud Function.
@@ -19,31 +20,33 @@ def run_beans_pipeline(request):
         request_json = request.form.to_dict()
 
     # Validate required parameters
-    required_params = ['config_values', 'parameter_values']
+    required_params = ["config_values", "parameter_values"]
     for param in required_params:
         if param not in request_json:
             return f"Missing required parameter: {param}", 400
 
-    config_values = request_json['config_values']
-    parameter_values = request_json['parameter_values']
+    config_values = request_json["config_values"]
+    parameter_values = request_json["parameter_values"]
 
     try:
-        project_id = config_values['project_id']
-        location = config_values['location']
-        staging_bucket=config_values['staging_bucket']
-        service_account=config_values['service_account']
-        pipeline_display_name = config_values['pipeline_display_name']
-        pipeline_repo = config_values['pipeline_repo']
-        pipeline_name = config_values['pipeline_name']
-        pipeline_tag=config_values['pipeline_tag']
+        project_id = config_values["project_id"]
+        location = config_values["location"]
+        staging_bucket = config_values["staging_bucket"]
+        service_account = config_values["service_account"]
+        pipeline_display_name = config_values["pipeline_display_name"]
+        pipeline_repo = config_values["pipeline_repo"]
+        pipeline_name = config_values["pipeline_name"]
+        pipeline_tag = config_values["pipeline_tag"]
     except KeyError as e:
-    # Handle the missing parameter error
-        return  f"Missing required parameter in config_params: {e}", 400
+        # Handle the missing parameter error
+        return f"Missing required parameter in config_params: {e}", 400
 
-    pipeline_root = f"https://{location}-kfp.pkg.dev/{project_id}/{pipeline_repo}/{pipeline_name}"
+    pipeline_root = (
+        f"https://{location}-kfp.pkg.***/{project_id}/{pipeline_repo}/{pipeline_name}"
+    )
 
     # Get pipeline definition from Registry
-    general_values ={key: config_values[key] for key in ['project_id', 'location']}
+    general_values = {key: config_values[key] for key in ["project_id", "location"]}
     parameter_values = parameter_values | general_values
 
     try:
@@ -54,8 +57,8 @@ def run_beans_pipeline(request):
         )
         job = aiplatform.PipelineJob(
             display_name=pipeline_display_name,
-            template_path=f"{pipeline_root}/{pipeline_tag}" ,
-            parameter_values= parameter_values,
+            template_path=f"{pipeline_root}/{pipeline_tag}",
+            parameter_values=parameter_values,
         )
         # If the pipeline job is created successfully, you can start it here
         print(
@@ -68,7 +71,9 @@ def run_beans_pipeline(request):
 
     except NotFound as e:
         if "template path not found" in str(e).lower():  # Case-insensitive check
-            print(f"Error: Pipeline template not found at '{pipeline_root}/{pipeline_tag}'. Please double-check the path.")
+            print(
+              f"Error: Pipeline template not found at '{pipeline_root}/{pipeline_tag}'. Please double-check the path.",
+            )
         else:
             print(f"An unexpected NotFound error occurred: {e}")
     except Exception as e:  # Catch any other potential errors
