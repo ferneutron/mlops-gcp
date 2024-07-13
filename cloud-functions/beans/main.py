2da6af64c19351aa16fbe6d21041b265f5f331f7
Ulises Jimenez <ulisesjimenez@google.com>
2024-07-12 19:31:13 -0600

    add error messages

--- cloud-functions/beans/main.py
+++ cloud-functions/beans/main.py
@@ -1,6 +1,13 @@
 import functions_framework
+import time
 from google.cloud import aiplatform
 from google.api_core.exceptions import NotFound
+
+
+MAX_ATTEMPS_STATUS = 6  # Wait for approximately 2 min
+WAIT_INTERVAL_SECONDS = 20  # 20 seconds between attempts
+RUNNING=aiplatform.gapic.PipelineState.PIPELINE_STATE_RUNNING
+FAILED=aiplatform.gapic.PipelineState.PIPELINE_STATE_FAILED
 
 
 @functions_framework.http
@@ -14,6 +21,13 @@ def run_beans_pipeline(request):
         Response object using `make_response`
         <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
     """
+    res_message={
+                "statusCode": 400,
+                "pipelineStatus": "FAILED",
+                "message": "",
+                "jobName": None,
+    }
+
     # Get the input parameters from the request
     request_json = request.get_json(silent=True)
     if request_json is None:
@@ -23,7 +37,8 @@ def run_beans_pipeline(request):
     required_params = ["config_values", "parameter_values"]
     for param in required_params:
         if param not in request_json:
-            return f"Missing required parameter: {param}", 400
+            res_message["message"]=f"Missing required parameters: {param}",
+            return res_message
 
     config_values = request_json["config_values"]
     parameter_values = request_json["parameter_values"]
@@ -39,10 +54,10 @@ def run_beans_pipeline(request):
         pipeline_tag = config_values["pipeline_tag"]
     except KeyError as e:
         # Handle the missing parameter error
-        return f"Missing required parameter in config_params: {e}", 400
+        res_message["message"]=f"Missing required parameter: {e}"
 
     pipeline_root = (
-        f"https://{location}-kfp.pkg.***/{project_id}/{pipeline_repo}/{pipeline_name}"
+        f"https://{location}-kfp.pkg.dev/{project_id}/{pipeline_repo}/{pipeline_name}"
     )
 
     # Get pipeline definition from Registry
@@ -70,12 +85,33 @@ def run_beans_pipeline(request):
 
     except NotFound as e:
         if "template path not found" in str(e).lower():  # Case-insensitive check
-            print(
-                f"Error: Pipeline template '{pipeline_root}/{pipeline_tag}' not found. Please double-check the path.",
-            )
+            res_message["message"]= \
+                f"Error: Pipeline template' {pipeline_root}/{pipeline_tag}'. Please double-check the path."
         else:
-            print(f"An unexpected NotFound error occurred: {e}")
+            res_message["message"]=f"An unexpected NotFound error occurred: {e}"
+        return res_message
     except Exception as e:  # Catch any other potential errors
-        print(f"An error occurred while creating the pipeline job: {e}")
+        res_message["message"]=f"An error occurred while creating the pipeline job: {e}"
+        return res_message
 
-    return f"{job.display_name} submitted successfully. Job State {job.state}", 200
+
+    # Wait for Running or Failed status before finalizing
+    attempt = 0
+    while job.state not in [RUNNING, FAILED]:
+        attempt += 1
+        if attempt > MAX_ATTEMPS_STATUS:
+            res_message["message"]=f"Pipeline with '{job.state}' did not run within the time limit."
+        time.sleep(WAIT_INTERVAL_SECONDS)
+
+    if job.state == RUNNING:
+        res_message["statusCode"]= 200
+        res_message["pipelineStatus"] = "RUNNING"
+        res_message["message"] = f"Pipeline job {job.display_name} is running."
+    elif job.state == FAILED:
+        res_message["statusCode"]= 400
+        res_message["pipelineStatus"] = "FAILED"
+        res_message["message"] = f"Pipeline job {job.display_name} failed."
+
+    res_message["jobName"] = job.display_name
+
+    return res_message
