fb8bf257941e0310c41659866b1cb2bad4b59e59
ferneutron <ferneutronn@gmail.com>
2024-07-23 19:04:49 -0600

    Validate change on main.tfç

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,3 +24,30 @@ resource "google_compute_instance" "terraform-instance" {
         }
     }
 }
+
+resource "google_service_account" "sa" {
+  account_id   = "mlops-test"
+  display_name = "MLOpsTestSA"
+  disabled     = false
+  project      = "gsd-ai-mx-ferneutron"
+
+  # Add the roles that the service account needs
+  roles = [
+    "roles/artifactregistry.writer",
+    "roles/bigquery.admin",
+    "roles/bigquery.readsessions.user",
+    "roles/cloudbuild.loggingServiceAgent",
+    "roles/cloudbuild.serviceAccount",
+    "roles/cloudbuild.tokenAccessor",
+    "roles/cloudbuild.workerpoolUser",
+    "roles/cloudfunctions.admin",
+    "roles/cloudfunctions.developer",
+    "roles/cloudrun.admin",
+    "roles/cloudrun.invoker",
+    "roles/developer.connect.user",
+    "roles/logging.writer",
+    "roles/iam.serviceAccountUser",
+    "roles/storage.admin",
+    "roles/vertexai.user",
+  ]
+}
