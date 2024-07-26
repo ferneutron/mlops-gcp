fe3ba78c0521cb6fc181d3fbceba68d0a8ea412b
ferneutron <ferneutronn@gmail.com>
2024-07-26 08:46:02 -0600

    Add terraform variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,23 @@
-variable "gcp_svc_key" {}
-variable "gcp_project" {}
-variable "gcp_region" {}
+variable "project_id" {
+    description = "project_id"
+    type = string
+    sensitive = true
+}
+
+variable "location" {
+    description = "location"
+    type = string
+    sensitive = true
+}
+
+variable "service_account_name" {
+    description = "service_account_name"
+    type = string
+    sensitive = true
+}
+
+variable "roles_list" {
+    description = "roles_list"
+    type = list(string)
+    sensitive = false
+}
